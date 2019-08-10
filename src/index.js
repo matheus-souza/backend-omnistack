1714ec6a4f35852874647484fdbdc2b0d3603c22
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-09 22:18:50 -0300

    Adicionado cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const server = express();
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://backomnistack:@souza123@cluster0-8ykdl.mongodb.n
     useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 

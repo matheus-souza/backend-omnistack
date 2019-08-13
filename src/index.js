81e7426d27b019c5de0959c1a5f3107e1f0fc912
Matheus H. de Souza <mh.matheussouza@gmail.com>
2019-08-13 01:57:57 -0300

    Implementando multiplos serviços de conexão

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const routes = require('./routes');
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+});
 
 mongoose.connect('mongodb+srv://backomnistack:@souza123@cluster0-8ykdl.mongodb.net/onministack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
